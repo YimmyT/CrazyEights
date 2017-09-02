51f2cb518f1068a56659214289b575c8c7217db6
YimmyT <holidaych1881@yahoo.com>
2017-09-02 18:40:02 -0500

    Initial commit

---
+++ app/src/main/java/jamisty/crazyeights/Card.java
@@ -0,0 +1,55 @@
+package jamisty.crazyeights;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by J. Evans on 8/23/2017.
+ */
+
+public class Card {
+
+    private int id;
+    private int suit;
+    private int rank;
+    private Bitmap bmp;
+    private int scoreValue;
+
+    public Card(int newId) {
+        id = newId;
+        suit = Math.round((id/100) * 100);
+        rank = id - suit;
+        if (rank == 8) {
+            scoreValue = 50;
+        } else if (rank == 14) {
+            scoreValue = 1;
+        } else if (rank > 9 && rank < 14) {
+            scoreValue = 10;
+        } else {
+            scoreValue = rank;
+        }
+    }
+
+    public void setBitmap(Bitmap newBitmap) {
+        bmp = newBitmap;
+    }
+
+    public Bitmap getBitmap() {
+        return bmp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public  int getScoreValue() {
+        return scoreValue;
+    }
+}
